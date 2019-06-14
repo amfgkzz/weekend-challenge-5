12d926d25251957423b48cf8131b1b04e993754a
Koua Xiong <kxiong517@gmail.com>
2019-06-14 13:59:23 -0500

    added input and button in form for first page

--- src/components/FirstPage/FirstPage.js
+++ src/components/FirstPage/FirstPage.js
@@ -2,11 +2,22 @@ import React, { Component } from "react";
 
 class FirstPage extends Component {
 
+    handleClickFeelings = (event) => {
+        event.preventDefault();
+        console.log('works!');
+    }
+
     render() {
 
         return(
             <>
-            <h1>First</h1>
+            <h1>HOW ARE YOU FEELING TODAY?</h1>
+            
+            <form onSubmit={this.handleClickFeelings}>
+            <input style={{width: 200}} placeholder="TYPE YOUR FEELINGS HERE!"/>
+            <button>Next</button>
+            </form>
+
             </>
         )
     }
