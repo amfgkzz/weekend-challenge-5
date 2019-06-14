4e9ac88b79272e6aa9e60179f90b6d172ce79c1c
Koua Xiong <kxiong517@gmail.com>
2019-06-14 14:02:15 -0500

    added input and button in form for second page

--- src/components/SecondPage/SecondPage.js
+++ src/components/SecondPage/SecondPage.js
@@ -2,11 +2,22 @@ import React, { Component } from "react";
 
 class SecondPage extends Component {
 
+    handleClick = (event) => {
+        event.preventDefault();
+        console.log('works also!');
+    }
+
     render() {
 
         return(
             <>
-            <h1>Second</h1>
+            <h1>HOW WELL ARE YOU UNDERSTANDING THE CONCEPTS?</h1>
+            
+            <form onSubmit={this.handleClick}>
+            <input style={{width: 200}} placeholder="Understanding?"/>
+            <button>Next</button>
+            </form>
+
             </>
         )
     }
